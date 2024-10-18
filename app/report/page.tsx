55680ed12d775dfae5b8bbede1bd0b22f9846889
SagaraBG3 <sagarabg3@gmail.com>
2024-10-19 00:53:18 +0530

    collection

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -10,7 +10,7 @@ import { useRouter } from 'next/navigation';
 import { toast } from 'react-hot-toast'
 
 const geminiApiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
-//const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
 const libraries: Libraries = ['places'];
 
